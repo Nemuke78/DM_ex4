c3e6c9a194c9d41ac7cf65f244257050524fc918
Nemuke78 <86813317+Nemuke78@users.noreply.github.com>
2021-07-02 11:44:16 +0900

    Initial revision

---
+++ ex4.py
@@ -0,0 +1,10 @@
+import random
+
+d1 = random.randint(1, 6)
+d2 = random.randint(1, 6)
+total = d1 + d2
+
+print("Rolling the dice...")
+print("Die 1: %d" % d1)
+print("Die 2: %d" % d2)
+print("Total value: %d" % total)
