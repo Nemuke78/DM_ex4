ee749d29a26ce65f10cabcfaf7fc81dd03db5142
Nemuke78 <86813317+Nemuke78@users.noreply.github.com>
2021-07-02 12:28:23 +0900

    Greetings added

--- ex4.py
+++ ex4.py
@@ -4,6 +4,11 @@ d1 = random.randint(1, 6)
 d2 = random.randint(1, 6)
 total = d1 + d2
 
+#名前を聞く
+name = input("What is your name? ")
+
+#ダイスの出力
+print("Hello, {0}!".format(name))
 print("Rolling the dice...")
 print("Die 1: %d" % d1)
 print("Die 2: %d" % d2)
