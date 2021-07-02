cbf448639634aa1e645f8b2e2e52c74ba24f2a4d
Nemuke78 <86813317+Nemuke78@users.noreply.github.com>
2021-07-02 13:06:04 +0900

    Won/Lost message modified

--- ex4.py
+++ ex4.py
@@ -8,3 +8,9 @@ print("Rolling the dice...")
 print("Die 1: %d" % d1)
 print("Die 2: %d" % d2)
 print("Total value: %d" % total)
+
+#勝ち負け判定
+if total > 7:
+    print(name + " won!")
+else:
+    print(name + " lost...")
